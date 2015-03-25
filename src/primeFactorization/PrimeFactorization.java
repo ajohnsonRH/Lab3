62f97979055578224860d480b517634f0a102100
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-03-25 12:59:16 -0400

    Added least amount of code to pass testNine

--- src/primeFactorization/PrimeFactorization.java
+++ src/primeFactorization/PrimeFactorization.java
@@ -6,8 +6,12 @@ public class PrimeFactorization {
 
 	public static ArrayList<Integer> generate(int i) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
 		for(; i % 2 == 0;i/=2) {
 			primes.add(2);
+		}
+		for(; i % 3 == 0;i/=3) {
+			primes.add(3);
 		}
 		if (i > 1) {
 			primes.add(i);
