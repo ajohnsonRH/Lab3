f4c283596c5621320278733bf8c79fb2870f1357
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-03-25 20:25:42 -0400

    Added least passing code for testFive

--- src/primeFactorization/PrimeNumberListGenerator.java
+++ src/primeFactorization/PrimeNumberListGenerator.java
@@ -1,15 +1,15 @@
 package primeFactorization;
 
-
 import java.util.ArrayList;
 
 public class PrimeNumberListGenerator {
 
 	public ArrayList<Integer> generateList(Integer inputNumber) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-	
-		for(int i=1;i<inputNumber;i++){
-			primes.add(i);
+		for (int i = 1; i < inputNumber; i++) {
+			if (i/2 >1) {
+			} else
+				primes.add(i);
 		}
 		return primes;
 	}
